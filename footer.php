f3be86f21bf6caa491a91283fee4cfcbde9470d7
cube201705 <python.isfun2017@gmail.com>
2017-07-06 22:39:07 -0400

    Delete footer.php

--- footer.php
+++
@@ -1,6 +0,0 @@
-            <footer>
-                <p>&copy; <?php echo date("Y"); ?> CST8285. All Rights Reserved.</p>
-            </footer>
-        </div><!-- End Wrapper -->
-    </body>
-</html>
